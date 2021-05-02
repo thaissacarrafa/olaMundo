7cc2d334b494934ed12489c3dfa9090eabf413bd
UbuntuVM <thaissacarrafa@gmail.com>
2021-05-02 00:23:10 -0300

    comentários

--- Programa.java
+++ Programa.java
@@ -2,9 +2,25 @@
 
 public class Programa {
 
+
 	public static void main(String[] args) {
 		System.out.println("ola mundo");
 	
 	}
 	}
+	
+	
+	/* declaração da Classe: "Class"
+	   Nome da Classe: "Programa"
+	   Método: Funções que o Java pode exercer -
+	   "public static void main(String[] args)         				System.out.println("ola mundo");}
+	   Modificadores de acesso do método: public static (não muda)
+	   Tipo de Retorno do Método: não retorna nada ( void)
+	   Nome do Método: Main
+	   Case sensitive: Lembrar de deixar as letras minúsculas
+	   Nosso código: System.out.println("ola mundo");
+	   Blocos: Entre chaves {}
+	   
+	  */ 
+	  
 	
